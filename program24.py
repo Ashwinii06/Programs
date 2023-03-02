dfe75c29b37f6e468cc5dc88a11e730bcd6da4c9
Ashwinii06 <124331267+Ashwinii06@users.noreply.github.com>
2023-03-02 10:19:05 +0530

    Add files via upload

---
+++ program24.py
@@ -0,0 +1,10 @@
+def isPalindrome(x: int) -> bool:
+ 
+    if x < 0:
+        return False
+    number = x
+    reverse = 0
+    while number:
+        reverse = reverse * 10 + number % 10
+        number //= 10
+    return x == reverse
